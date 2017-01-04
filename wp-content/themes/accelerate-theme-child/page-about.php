a3425b9b5943f9689d37d6c6b237ff81362def56
Taylor Haigler <taylorlynn@haigler.net>
2017-01-03 20:42:29 -0500

    editing about page

--- wp-content/themes/accelerate-theme-child/page-about.php
+++ wp-content/themes/accelerate-theme-child/page-about.php
@@ -1,4 +1,9 @@
-<?php About Page ?>
+
+<?php
+/*
+Template Name: About Page
+*/
+?>
 
 <?php
 /**
@@ -16,7 +21,44 @@ get_header(); ?>
             <?php while ( have_posts() ) : the_post(); ?>
                 <h2><?php the_title(); ?></h2>
                 <?php the_content(); ?>
-                <h3>hello</h3>
+                <div class="about-banner">
+                    <img src="accelerate-theme-child/img/taxi-photo.jpg" alt="Taxi">
+                    <p><span class="green">Accelerate</span> is a strategy and marketing agency located in the heart of NYC. Our goal is to build business by making our clients visible and making their customers smile.</p>
+                </div>
+
+                <div class="about_services"> 
+                    <h3>Our Services</h3>
+                    <p>We take pride in our clients and the content we create for them. 
+                    <br> Here's a brief overview of our offered services.
+                    </p>
+                </div>
+                <div class="about_content">
+                    <img src="img/img-content.png" alt="Content Icon">
+                    <h2>Content Strategy</h2>
+                    <p>Bacon ipsum dolor sit amet strip steak jowl pancetta, cow turkey salami sausage fatback boudin biltong frankfurter shoulder pork turducken spare ribs. Ground rount t-bone swine shoulder, cow bacon sausage turkey meatloaf. Tounge ball tip corned beef turkey. Leberkas shoulder pork chop doner venison meatball pig chicken spare.</p>
+                </div>
+                 <div class="about_influencer">
+                    <h2>Influencer Mapping</h2>
+                    <p>Bacon ipsum dolor sit amet strip steak jowl pancetta, cow turkey salami sausage fatback boudin biltong frankfurter shoulder pork turducken spare ribs. Ground rount t-bone swine shoulder, cow bacon sausage turkey meatloaf. Tounge ball tip corned beef turkey. Leberkas shoulder pork chop doner venison meatball pig chicken spare.</p>
+                    <img src="img/img-influencer.png" alt="Influencer Icon">
+                </div>
+                <div class="about_social">
+                    <img src="img/img-social.png" alt="Social Icon">
+                    <h2>Social Media Strategy</h2>
+                    <p>Bacon ipsum dolor sit amet strip steak jowl pancetta, cow turkey salami sausage fatback boudin biltong frankfurter shoulder pork turducken spare ribs. Ground rount t-bone swine shoulder, cow bacon sausage turkey meatloaf. Tounge ball tip corned beef turkey. Leberkas shoulder pork chop doner venison meatball pig chicken spare.</p>
+                </div>
+                <div class="about_design">
+                    <h2>Design & Development</h2>
+                    <p>Bacon ipsum dolor sit amet strip steak jowl pancetta, cow turkey salami sausage fatback boudin biltong frankfurter shoulder pork turducken spare ribs. Ground rount t-bone swine shoulder, cow bacon sausage turkey meatloaf. Tounge ball tip corned beef turkey. Leberkas shoulder pork chop doner venison meatball pig chicken spare.</p>
+                    <img src="img/img-design.png" alt="Design Icon">
+                </div>
+                </div>
+               
+
+                    
+       
+
+
             <?php endwhile; // end of the loop. ?>
 
         </div><!-- #content -->
